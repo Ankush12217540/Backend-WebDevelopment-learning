30b71712aa223b71c28f96b5b8ddd4738e57dce6
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-05-30 19:18:54 +0530

    learning nodeJs

---
+++ Codes while learning/lib.js
@@ -0,0 +1,11 @@
+exports.sum=(a,b)=>{
+    return a+b;
+}
+exports.diff=(a,b)=>{
+    return a-b;
+}
+const a=5;
+console.log("hello");
+console.log(a+42524);
+// exports.sum=sum;
+// exports.diff=diff;
